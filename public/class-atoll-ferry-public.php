c48730fc51acdff2abaab551f3d5506551c8fd1c
Mondre <mondrey.m@gmail.com>
2023-11-10 23:00:34 +0500

    Populated ferry data and building frontend

--- public/class-atoll-ferry-public.php
+++ public/class-atoll-ferry-public.php
@@ -52,6 +52,442 @@ class Atoll_Ferry_Public {
 		$this->atoll_ferry = $atoll_ferry;
 		$this->version = $version;
 
+		add_shortcode('ferryfinder', array($this, 'ferry_schedule_finder'));
+
+		add_action('wp_ajax_get_schedule_for_departure_island', array($this, 'get_schedule_for_departure_island_callback'));
+		add_action('wp_ajax_nopriv_get_schedule_for_departure_island', array($this, 'get_schedule_for_departure_island_callback'));
+
+	}
+
+	public function ferry_schedule_finder() {
+
+		$ferry_schedules = self::get_ferry_schedule();
+ob_start();
+?>
+    <div id="speedboat-search">
+        <label for="departure-island">Departure Island:</label>
+        <select id="departure-island">
+            <!-- Add departure islands dynamically if needed -->
+            <?php
+			echo self::create_select_list( $ferry_schedules );
+			?>
+        </select>
+
+        <button id="search-btn">Search</button>
+
+        <div id="results"></div>
+    </div>
+<?php
+return ob_get_clean();
+
+	}
+
+	public function create_select_list($data) {
+		$islands = [];
+	
+		// Loop through the $data array to collect unique island names
+		foreach ($data as $departureIsland => $arrivalData) {
+			$islands[] = $departureIsland;
+			foreach ($arrivalData as $arrivalIsland => $schedule) {
+				$islands[] = $arrivalIsland;
+			}
+		}
+	
+		// Remove duplicates and sort the island names
+		$uniqueIslands = array_unique($islands);
+		sort($uniqueIslands);
+	
+		$selectList = '';
+		// Create a select list
+		foreach ($uniqueIslands as $island) {
+			$selectList .= '<option value="' . $island . '">' . $island . '</option>';
+		}
+	
+		return $selectList;
+	}
+
+	public function get_schedule_for_departure_island_callback() {
+		$selectedIsland = $_POST['selectedIsland'];
+		global $data; // Assuming $data is a global variable containing your schedule data
+	
+		// Call the existing PHP function to get the schedule
+		$scheduleOutput = self::get_schedule_for_departure_island($selectedIsland, $data);
+	
+		echo $scheduleOutput;
+	
+		wp_die(); // Always include this line to terminate the script
+	}	
+
+	public function get_schedule_for_departure_island($selectedIsland, $data) {
+		$scheduleOutput = '';
+	
+		// Check if the selected island exists in the $data array
+		if (array_key_exists($selectedIsland, $data)) {
+			// Loop through the $data array for the selected departure island
+			foreach ($data[$selectedIsland] as $destinationIsland => $schedule) {
+				$daysOfWeek = key($schedule);
+				$time = current($schedule);
+	
+				// Build the output string
+				$scheduleOutput .= "Departure Island: $selectedIsland, Destination Island: $destinationIsland, Days: $daysOfWeek, Time: $time<br>";
+			}
+		} else {
+			$scheduleOutput = "No schedule found for the selected departure island.";
+		}
+	
+		return $scheduleOutput;
+	}
+
+	public function get_ferry_schedule() {
+
+		$data = array();
+// 301
+
+$data['Himandhoo']['Maalhos'] = [
+	'Saturday, Sunday, Monday, Tuesday, Wednesday, Thursday' => '6:30am,7:00am',
+];
+
+$data['Maalhos']['Feridhoo'] = [
+	'Saturday, Sunday, Monday, Tuesday, Wednesday, Thursday' => '7:05am,7:30am',
+];
+
+$data['Feridhoo']['Mathiveri'] = [
+	'Saturday, Sunday, Monday, Tuesday, Wednesday, Thursday' => '7:35am,8:30am',
+];
+
+$data['Mathiveri']['Bodufolhudhoo'] = [
+	'Saturday, Sunday, Monday, Tuesday, Wednesday, Thursday' => '8:35am,8:55am',
+];
+
+$data['Bodufolhudhoo']['Ukulhas'] = [
+	'Saturday, Sunday, Monday, Tuesday, Wednesday, Thursday' => '9:00am,9:35am',
+];
+
+$data['Ukulhas']['Rasdhoo'] = [
+	'Saturday, Sunday, Monday, Tuesday, Wednesday, Thursday' => '10:00am,10:55am',
+];
+
+$data['Rasdhoo']['Ukulhas'] = [
+	'Saturday, Sunday, Monday, Tuesday, Wednesday, Thursday' => '13:00pm,13:55pm',
+];
+
+$data['Ukulhas']['Bodufolhudhoo'] = [
+	'Saturday, Sunday, Monday, Tuesday, Wednesday, Thursday' => '14:00pm,14:35pm',
+];
+
+$data['Bodufolhudhoo']['Mathiveri'] = [
+	'Saturday, Sunday, Monday, Tuesday, Wednesday, Thursday' => '14:40pm,15:00pm',
+];
+
+$data['Mathiveri']['Feridhoo'] = [
+	'Saturday, Sunday, Monday, Tuesday, Wednesday, Thursday' => '15:05pm,16:05pm',
+];
+
+$data['Feridhoo']['Maalhos'] = [
+	'Saturday, Sunday, Monday, Tuesday, Wednesday, Thursday' => '16:10pm,16:35pm',
+];
+
+$data['Maalhos']['Himandhoo'] = [
+	'Saturday, Sunday, Monday, Tuesday, Wednesday, Thursday' => '16:40pm,17:10pm',
+];
+
+
+// 302
+
+
+$data['Mandhoo']['Ku’nburudhoo'] = [
+	'Saturday, Sunday, Monday, Tuesday, Wednesday, Thursday' => '6:30am,8:05am',
+];
+
+$data['Ku’nburudhoo']['Mahibadhoo'] = [
+	'Saturday, Sunday, Monday, Tuesday, Wednesday, Thursday' => '8:10am,8:30am',
+];
+
+$data['Mahibadhoo']['Hangnaameedhoo'] = [
+	'Saturday, Sunday, Monday, Tuesday, Wednesday, Thursday' => '8:35am,9:15am',
+];
+
+$data['Hangnaameedhoo']['Omadhoo'] = [
+	'Saturday, Sunday, Monday, Tuesday, Wednesday, Thursday' => '9:20am,9:50am',
+];
+
+$data['Omadhoo']['Mahibadhoo'] = [
+	'Saturday, Sunday, Monday, Tuesday, Wednesday, Thursday' => '9:55am,10:10am',
+];
+
+$data['Mahibadhoo']['Omadhoo'] = [
+	'Saturday, Sunday, Monday, Tuesday, Wednesday, Thursday' => '13:30pm,13:45pm',
+];
+
+$data['Omadhoo']['Hangnaameedhoo'] = [
+	'Saturday, Sunday, Monday, Tuesday, Wednesday, Thursday' => '13:50pm,14:20pm',
+];
+
+$data['Hangnaameedhoo']['Mahibadhoo'] = [
+	'Saturday, Sunday, Monday, Tuesday, Wednesday, Thursday' => '14:25pm,15:05pm',
+];
+
+$data['Mahibadhoo']['Ku’nburudhoo'] = [
+	'Saturday, Sunday, Monday, Tuesday, Wednesday, Thursday' => '15:10pm,15:30pm',
+];
+
+$data['Ku’nburudhoo']['Mandhoo'] = [
+	'Saturday, Sunday, Monday, Tuesday, Wednesday, Thursday' => '15:35pm,17:10pm',
+];
+
+
+// 303
+
+$data['Thoddoo']['Rasdhoo'] = [
+    'Saturday,Tuesday' => '6:30am,8:10am',
+];
+
+$data['Rasdhoo']['Thoddoo'] = [
+    'Saturday,Tuesday' => '15:10pm,16:20pm',
+];
+		
+$data['Thoddoo']['Rasdhoo'] = [
+    'Sunday, Wednesday' => '6:30am,7:40am',
+];
+
+$data['Rasdhoo']['Ukulhas'] = [
+    'Sunday, Wednesday' => '7:45am,8:35am',
+];
+
+$data['Ukulhas']['Rasdhoo'] = [
+    'Sunday, Wednesday' => '9:45am,10:35am',
+];
+
+$data['Rasdhoo']['Male’'] = [
+    'Sunday, Wednesday' => '11:00am,14:10pm',
+];
+
+
+$data['Male’']['Rasdhoo'] = [
+    'Monday, Thursday' => '9:00am,12:10pm',
+];
+
+$data['Rasdhoo']['Ukulhas'] = [
+    'Monday, Thursday' => '12:15pm,13:05pm',
+];
+
+$data['Ukulhas']['Rasdhoo'] = [
+    'Monday, Thursday' => '14:00pm,14:50pm',
+];
+
+$data['Rasdhoo']['Thoddoo'] = [
+    'Monday, Thursday' => '15:10pm,16:20pm',
+];
+
+// 304
+
+$data['Fenfushi']['Maamigili'] = [
+    'Saturday, Sunday, Monday, Tuesday, Wednesday, Thursday' => '6:30am,6:55am',
+];
+
+$data['Maamigili']['Dhidhdhoo'] = [
+    'Saturday, Sunday, Monday, Tuesday, Wednesday, Thursday' => '7:00am,7:25am',
+];
+
+$data['Dhidhdhoo']['Dhigurah'] = [
+    'Saturday, Sunday, Monday, Tuesday, Wednesday, Thursday' => '7:30am,8:00am',
+];
+
+$data['Dhigurah']['Dha’ngethi'] = [
+    'Saturday, Sunday, Monday, Tuesday, Wednesday, Thursday' => '8:05am,8:35am',
+];
+
+$data['Dha’ngethi']['Mahibadhoo'] = [
+    'Saturday, Sunday, Monday, Tuesday, Wednesday, Thursday' => '8:40am,9:40am',
+];
+
+$data['Mahibadhoo']['Dha’ngethi'] = [
+    'Saturday, Sunday, Monday, Tuesday, Wednesday, Thursday' => '13:30pm,14:30pm',
+];
+
+$data['Dha’ngethi']['Dhigurah'] = [
+    'Saturday, Sunday, Monday, Tuesday, Wednesday, Thursday' => '14:35pm,15:05pm',
+];
+
+$data['Dhigurah']['Dhidhdhoo'] = [
+    'Saturday, Sunday, Monday, Tuesday, Wednesday, Thursday' => '15:10pm,15:40pm',
+];
+
+$data['Dhidhdhoo']['Maamigili'] = [
+    'Saturday, Sunday, Monday, Tuesday, Wednesday, Thursday' => '15:45pm,16:10pm',
+];
+
+$data['Maamigili']['Fenfushi'] = [
+    'Saturday, Sunday, Monday, Tuesday, Wednesday, Thursday' => '16:15pm,16:40pm',
+];
+
+// 305
+
+$data['Male']['Mahibadhoo'] = [
+    'Saturday, Monday, Wednesday' => '8:30am,12:40pm',
+];
+$data['Mahibadhoo']['Male'] = [
+    'Sunday, Tuesday, Thursday' => '11:00am,15:20pm',
+];
+
+
+// 306
+$data['Rakeedhoo']['Keyodhoo'] = [
+    'Saturday, Monday, Wednesday' => '7:00am,8:10am',
+];
+
+$data['Keyodhoo']['Felidhoo'] = [
+    'Saturday, Monday, Wednesday' => '8:20am,8:30am',
+];
+
+$data['Felidhoo']['Thinadhoo'] = [
+    'Saturday, Monday, Wednesday' => '8:40am,8:50am',
+];
+
+$data['Thinadhoo']['Fulidhoo'] = [
+    'Saturday, Monday, Wednesday' => '9:00am,10:30am',
+];
+
+$data['Fulidhoo']['K.Maafushi'] = [
+    'Saturday, Monday, Wednesday' => '10:45am,12:25pm',
+];
+
+$data['K.Maafushi']['Male'] = [
+    'Saturday, Monday, Wednesday' => '12:35pm,2:05pm',
+];
+
+
+$data['Male']['K.Maafushi'] = [
+    'Sunday, Tuesday, Thursday' => '10:00am,11:30am',
+];
+
+$data['K.Maafushi']['Fulidhoo'] = [
+    'Sunday, Tuesday, Thursday' => '11:35am,1:20pm',
+];
+
+$data['Fulidhoo']['Thinadhoo'] = [
+    'Sunday, Tuesday, Thursday' => '1:30pm,3:00pm',
+];
+
+$data['Thinadhoo']['Felidhoo'] = [
+    'Sunday, Tuesday, Thursday' => '3:05pm,3:15pm',
+];
+
+$data['Felidhoo']['Keyodhoo'] = [
+    'Sunday, Tuesday, Thursday' => '3:20pm,3:30pm',
+];
+
+$data['Keyodhoo']['Rakeedhoo'] = [
+    'Sunday, Tuesday, Thursday' => '3:35pm,4:50pm',
+];
+
+// 307
+$data['Kaashidhoo']['Gaafaru'] = [
+    'Saturday, Monday, Wednesday' => '7:00am,8:20am',
+];
+
+$data['Gaafaru']['Male'] = [
+    'Saturday, Monday, Wednesday' => '8:30am,11:50am',
+];
+
+$data['Male']['Gaafaru'] = [
+    'Sunday, Tuesday, Thursday' => '10:45am,12:25pm',
+];
+
+$data['Gaafaru']['Kaashidhoo'] = [
+    'Sunday, Tuesday, Thursday' => '12:35pm,3:20pm',
+];
+
+// 308
+
+$data['Dhiffushi']['Thulusdhoo'] = [
+    'Saturday, Sunday, Monday, Tuesday, Wednesday, Thursday' => '6:30am,7:05am',
+];
+
+$data['Thulusdhoo']['Huraa'] = [
+    'Saturday, Sunday, Monday, Tuesday, Wednesday, Thursday' => '7:10am,7:35am',
+];
+
+$data['Huraa']['Himmafushi'] = [
+    'Saturday, Sunday, Monday, Tuesday, Wednesday, Thursday' => '7:40am,7:55am',
+];
+
+$data['Himmafushi']['Male'] = [
+    'Saturday, Sunday, Monday, Tuesday, Wednesday, Thursday' => '8:00am,8:40am',
+];
+
+$data['Male']['Himmafushi'] = [
+    'Saturday, Sunday, Monday, Tuesday, Wednesday, Thursday' => '14:30pm,15:10pm',
+];
+
+$data['Himmafushi']['Huraa'] = [
+    'Saturday, Sunday, Monday, Tuesday, Wednesday, Thursday' => '15:15pm,15:30pm',
+];
+
+$data['Huraa']['Thulusdhoo'] = [
+    'Saturday, Sunday, Monday, Tuesday, Wednesday, Thursday' => '15:35pm,16:00pm',
+];
+
+$data['Thulusdhoo']['Dhiffushi'] = [
+    'Saturday, Sunday, Monday, Tuesday, Wednesday, Thursday' => '16:05pm,16:40pm',
+];
+
+
+// 309
+
+$data['Guraidhoo']['Maafushi'] = [
+    'Saturday, Sunday, Monday, Tuesday, Wednesday, Thursday' => '7:00am,7:20am',
+];
+
+$data['Maafushi']['Gulhi'] = [
+    'Saturday, Sunday, Monday, Tuesday, Wednesday, Thursday' => '7:25am,7:45am',
+];
+
+$data['Gulhi']['Male'] = [
+    'Saturday, Sunday, Monday, Tuesday, Wednesday, Thursday' => '7:50am,9:05am',
+];
+
+$data['Male']['Gulhi'] = [
+    'Saturday, Sunday, Monday, Tuesday, Wednesday, Thursday' => '15:00pm,16:15pm',
+];
+
+$data['Gulhi']['Maafushi'] = [
+    'Saturday, Sunday, Monday, Tuesday, Wednesday, Thursday' => '16:20pm,16:40pm',
+];
+
+$data['Maafushi']['Guraidhoo'] = [
+    'Saturday, Sunday, Monday, Tuesday, Wednesday, Thursday' => '16:45pm,17:05pm',
+];
+
+// 310
+
+$data['Feridhoo']['Maalhos'] = [
+    'Saturday, Sunday, Monday, Tuesday, Wednesday, Thursday' => '6:30am,6:55am',
+];
+
+$data['Maalhos']['Himandhoo'] = [
+    'Saturday, Sunday, Monday, Tuesday, Wednesday, Thursday' => '7:00am,7:30am',
+];
+
+$data['Himandhoo']['Adh.Mahibadhoo'] = [
+    'Saturday, Sunday, Monday, Tuesday, Wednesday, Thursday' => '7:35am,9:10am',
+];
+
+$data['Adh.Mahibadhoo']['Himandhoo'] = [
+    'Saturday, Sunday, Monday, Tuesday, Wednesday, Thursday' => '14:00pm,15:35pm',
+];
+
+$data['Himandhoo']['Maalhos'] = [
+    'Saturday, Sunday, Monday, Tuesday, Wednesday, Thursday' => '15:40pm,16:10pm',
+];
+
+$data['Maalhos']['Feridhoo'] = [
+    'Saturday, Sunday, Monday, Tuesday, Wednesday, Thursday' => '16:15pm,16:40pm',
+];
+
+
+return $data;
+
 	}
 
 	/**
--- public/js/atoll-ferry-public.js
+++ public/js/atoll-ferry-public.js
@@ -29,4 +29,21 @@
 	 * practising this, we should strive to set a better example in our own work.
 	 */
 
+	$('#island-select').change(function () {
+		var selectedIsland = $(this).val();
+
+		$.ajax({
+			type: 'POST',
+			url: ajax_object.ajax_url,
+			data: {
+				action: 'get_schedule_for_departure_island',
+				selectedIsland: selectedIsland,
+			},
+			success: function (response) {
+				// Display the response in a container (e.g., div with id 'schedule-output')
+				$('#schedule-output').html(response);
+			},
+		});
+	});
+
 })( jQuery );

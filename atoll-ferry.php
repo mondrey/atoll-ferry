39013bc9a8fc130b1e16f3cd045bf347d7f6cce5
Mondre <mondrey.m@gmail.com>
2023-11-17 04:04:26 +0500

    Styled days section

--- atoll-ferry.php
+++ atoll-ferry.php
@@ -3,14 +3,14 @@
 /**
  *
  * @link              https://github.com/mondrey/atoll-ferry
- * @since             1.0.4
+ * @since             1.0.5
  * @package           Atoll Ferry
  *
  * @wordpress-plugin
  * Plugin Name:       Atoll Ferry
  * Plugin URI:        https://github.com/mondrey/atoll-ferry
  * Description:       Maldives transport information plugin
- * Version:           1.0.4
+ * Version:           1.0.5
  * Author:            Mohamed Musthafa
  * Author URI:        https://github.com/mondrey/
  * License:           GPL-2.0+
